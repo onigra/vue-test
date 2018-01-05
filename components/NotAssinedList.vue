61443c581e7101e3925e9e13bbb5ef33e40fb020
onigra <3280467rec@gmail.com>
2018-01-05 14:44:04 +0900

    [from now] 2018/01/05 14:44:04
    diff --git a/components/NotAssinedList.vue b/components/NotAssinedList.vue
    new file mode 100644
    index 0000000..c10dcab
    --- /dev/null
    +++ b/components/NotAssinedList.vue
    @@ -0,0 +1,20 @@
    +<<template>
    +  <div>
    +    <ul>
    +      <p>ヘルパー未アサイン</p>
    +      <li v-for="(name, index) in notAssinedList" :key="index">
    +        {{ name }}
    +      </li>
    +    </ul>
    +  </div>
    +</template>
    +
    +<<script>
    +export default {
    +  data() {
    +    return {
    +      notAssinedList: ['亀仙人', 'ジョセフ・ジョースター', 'アイザック=ネテロ']
    +    }
    +  }
    +}
    +</script>
    diff --git a/pages/cal.vue b/pages/cal.vue
    new file mode 100644
    index 0000000..def8d9a
    --- /dev/null
    +++ b/pages/cal.vue
    @@ -0,0 +1,13 @@
    +<<template>
    +  <NotAssinedList />
    +</template>
    +
    +<script>
    +import NotAssinedList from '~/components/NotAssinedList.vue'
    +
    +export default {
    +  components: {
    +    NotAssinedList,
    +  },
    +}
    +</script>

---
+++ components/NotAssinedList.vue
@@ -0,0 +1,20 @@
+<<template>
+  <div>
+    <ul>
+      <p>ヘルパー未アサイン</p>
+      <li v-for="(name, index) in notAssinedList" :key="index">
+        {{ name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<<script>
+export default {
+  data() {
+    return {
+      notAssinedList: ['亀仙人', 'ジョセフ・ジョースター', 'アイザック=ネテロ']
+    }
+  }
+}
+</script>
---
+++ pages/cal.vue
@@ -0,0 +1,13 @@
+<<template>
+  <NotAssinedList />
+</template>
+
+<script>
+import NotAssinedList from '~/components/NotAssinedList.vue'
+
+export default {
+  components: {
+    NotAssinedList,
+  },
+}
+</script>
