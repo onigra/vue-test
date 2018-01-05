21c4689b0f7d6a9d092d6658b9de409bb54249ff
onigra <3280467rec@gmail.com>
2018-01-05 15:24:22 +0900

    [from now] 2018/01/05 15:24:22
    diff --git a/pages/todo.vue b/pages/todo.vue
    index 767ca04..3eb0b17 100644
    --- a/pages/todo.vue
    +++ b/pages/todo.vue
    @@ -1,10 +1,10 @@
    -<<template>
    +<template>
       <div>
         <input v-model="title" placeholder="title">
         <input v-model="description" placeholder="description">
         <button @click="addTodo">add</button>
         <ul>
    -      <li v-for="(todo, index) in todos">
    +      <li v-for="(todo, index) in todos" :key="index">
             {{ todo.title }} : {{ todo.description }}
             <button @click="removeTodo(index)">delete</button>
           </li>
    @@ -12,22 +12,25 @@
       </div>
     </template>
    
    -<<script>
    +<script>
     export default {
       data() {
         return { title: '', description: '' }
       },
       computed: {
    -    todos() { return this.$store.state.todo.list }
    +    todos() {
    +      return this.$store.state.todo.list
    +    },
       },
       methods: {
         addTodo(event) {
           this.$store.commit('todo/add', { title: this.title, description: this.description })
    -      this.title = ''; this.description = ''
    +      this.title = ''
    +      this.description = ''
         },
         removeTodo(index) {
           this.$store.commit('todo/rm', index)
    -    }
    -  }
    +    },
    +  },
     }
     </script>

--- pages/todo.vue
+++ pages/todo.vue
@@ -1,10 +1,10 @@
-<<template>
+<template>
   <div>
     <input v-model="title" placeholder="title">
     <input v-model="description" placeholder="description">
     <button @click="addTodo">add</button>
     <ul>
-      <li v-for="(todo, index) in todos">
+      <li v-for="(todo, index) in todos" :key="index">
         {{ todo.title }} : {{ todo.description }}
         <button @click="removeTodo(index)">delete</button>
       </li>
@@ -12,22 +12,25 @@
   </div>
 </template>
 
-<<script>
+<script>
 export default {
   data() {
     return { title: '', description: '' }
   },
   computed: {
-    todos() { return this.$store.state.todo.list }
+    todos() {
+      return this.$store.state.todo.list
+    },
   },
   methods: {
     addTodo(event) {
       this.$store.commit('todo/add', { title: this.title, description: this.description })
-      this.title = ''; this.description = ''
+      this.title = ''
+      this.description = ''
     },
     removeTodo(index) {
       this.$store.commit('todo/rm', index)
-    }
-  }
+    },
+  },
 }
 </script>
